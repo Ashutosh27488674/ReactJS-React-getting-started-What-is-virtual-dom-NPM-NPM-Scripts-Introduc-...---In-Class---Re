bcec651f3b4679e81fe30a282538a2a1f7d228be
Ashutosh Kumar <90816826+Ashutosh27488674@users.noreply.github.com>
2022-06-03 11:13:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div id="main">
+      <h1 data-ns-test="project-name">Amazon Clone</h1>
+      <h2 data-ns-test="project-description">Description of Amazon Clone.</h2>
+    </div>
+  );
+}
